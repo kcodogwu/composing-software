0cdae7ef3422bdc3f1e4bfd064869e2f3e7a364a
Kingson Chinedu Odogwu <kcodogwu@gmail.com>
2017-09-28 21:26:50 +0100

    Composing Software: Why Learn Functional Programming in JavaScript?

---
+++ 3-why-learn-fp-in-js.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// anonymous functions and concise lamba syntax
+const y = x => x * 2;
+
+// mutation is a change to data structure that happens in-place
+const foo = { bar: 'baz' };
+
+foo.bar = 'qux'; // mutation
